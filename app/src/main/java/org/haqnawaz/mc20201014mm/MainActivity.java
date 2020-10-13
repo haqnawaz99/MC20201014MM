754e502fca598cfd92fe113e6ccbf1c5449ffe4b
Haq Nawaz <haqnawaz99@yahoo.com>
2020-10-14 01:57:19 +0500

    Initial commit

---
+++ app/src/main/java/org/haqnawaz/mc20201014mm/MainActivity.java
@@ -0,0 +1,31 @@
+package org.haqnawaz.mc20201014mm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Animation (View view){
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        //imageView.animate().alpha(0).setDuration(500);
+
+       // imageView.animate().translationXBy(-500).setDuration(500);
+
+       // imageView.animate().rotation(720).setDuration(2000);
+
+        imageView.animate().rotation(720).alpha(0).setDuration(2000);
+
+
+    }
+}
